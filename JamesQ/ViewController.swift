ca3303b42a4ec44db4c627868d938dcbb766c011
netwire88 <netwire88@gmail.com>
2014-06-25 21:00:46 -0400

    Part 2

--- JamesQ/ViewController.swift
+++ JamesQ/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableData: NSArray = []
+    @IBOutlet var appsTableView : UITableView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("Clash of Clans")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView!, numberOfRowsInSection section:    Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.text = rowData["trackName"] as String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        var imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel.text = formattedPrice
+        
+        return cell
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            println("Task completed")
+            if(error) {
+                // If there is an error in the web request, print it to the console
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            //The closure method here uses the NSJSONSerialization class to convert our raw data in to useful Dictionary objects by deserializing the results from iTunes.
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            if(err?) {
+                // If there is an error parsing JSON, print it to the console
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            var results: NSArray = jsonResult["results"] as NSArray
+            
+            //Go back in to the main thread, and reload the table view
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableData = results
+                self.appsTableView.reloadData()
+                })
+            })
+        task.resume()  //begins the request
+    }
+    
 }
 
