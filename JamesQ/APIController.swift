96a3b711c811128a02e27bc6e3d12e243825cb9d
netwire88 <netwire88@gmail.com>
2014-06-25 21:40:35 -0400

    Finished Part 3

---
+++ JamesQ/APIController.swift
@@ -0,0 +1,51 @@
+//
+//  APIController.swift
+//  JamesQ
+//
+//  Created by Dean Chen on 6/25/14.
+//  Copyright (c) 2014 Corklabs. All rights reserved.
+//
+
+import UIKit
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController: NSObject {
+    
+    //include a delegate object, and to call this method when the connection finished loading some API data
+    var delegate: APIControllerProtocol?
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
+            println("Task completed")
+            if(error) {
+                // If there is an error in the web request, print it to the console
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            //The closure method here uses the NSJSONSerialization class to convert our raw data in to useful Dictionary objects by deserializing the results from iTunes.
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            if(err?) {
+                // If there is an error parsing JSON, print it to the console
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            var results: NSArray = jsonResult["results"] as NSArray
+            // Now send the JSON result to our delegate object
+            self.delegate?.didReceiveAPIResults(jsonResult)
+            })
+        task.resume()  //begins the request
+    }
+
+}
